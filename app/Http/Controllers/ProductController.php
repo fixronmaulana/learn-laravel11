d46fc46b2e207e5b112bd856e05ef1cb1bd14452
Fixron Maulana <fikronmaulana27@gmail.com>
2025-01-19 23:09:20 +0700

    Add fungsi create & store di ProductCotroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -14,5 +16,38 @@ class ProductController extends Controller
 
          //render view with products
          return view('products.index', compact('products'));
+    }
+
+    public function create() : View
+    {
+        return view('product.create');
+    }
+
+    public function store(Request $request) : RedirectResponse
+    {
+        //validate form
+        $request->validate([
+            'image' => 'requried|image|jpeg,jpg,png|max:2048',
+            'title' => 'required|min:5',
+            'descriptipn' => 'required|min:10',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/products', $image->hasName());
+
+        //create product
+        Product::create([
+            'image' => $image->hasName(),
+            'titile' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock
+        ]);
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['succes' => 'Data berhasil disimpan!']);
     }
 }
